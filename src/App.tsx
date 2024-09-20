4501681090c274a6c52cf13880085d8654cedb6c
rezha4 <rezha.bahari@gmail.com>
2024-09-20 09:05:20 +0700

    setup vite react ts swc, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl">expetracke</h1>
+      <h2 className="text-center text-2xl">expense tracking web app</h2>
+    </>
+  );
+}
+
+export default App;
