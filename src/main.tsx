21c6f5a3e2df87fa07517c0012a1905d7e445667
rezha4 <rezha.bahari@gmail.com>
2024-09-24 10:18:14 +0700

    feat: createBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,48 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./routes/app.tsx";
+import Home from "./routes/home.tsx";
+import Report from "./routes/report.tsx";
+import Profile from "./routes/profile.tsx";
+import Register from "./routes/register.tsx";
+import Dashboard from './routes/dashboard.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/home",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "report",
+        element: <Report />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
